96570882b10df1eb4dddbc4c9d22e13631a756e9
Jacob Lewallen <jlewallen@gmail.com>
2016-11-30 18:12:56 -0500

    Tinkering with some help ledger functions.

--- .spacemacs.d/layers/jacob-finance/packages.el
+++ .spacemacs.d/layers/jacob-finance/packages.el
@@ -15,10 +15,30 @@
         ledger-mode
         ))
 
-
-(defun jacob-ledger-all-accounts ()
+(defun jacob-ledger-find-accounts-in-buffer ()
   (setq pcomplete-args '(""))
   (ledger-find-accounts-in-buffer)
+  )
+
+(defun jacob-strip-booleans (tree)
+  (if (atom tree)
+      tree
+    (mapcar (lambda (node) (jacob-strip-booleans node))
+            (remove t tree))))
+
+(defun jacob-expand-account-paths (tree)
+  (cond
+   ((and (eq 1 (length tree)) (atom (car tree))) tree)
+   ((and (eq 1 (length tree)) t) (jacob-expand-account-paths (car tree)))
+   ((atom (car tree))
+    (mapcar (lambda (node) (concat (car tree) ":" node)) (jacob-expand-account-paths (cdr tree)))
+    )
+   (t (apply 'append
+             (mapcar (lambda (node) (jacob-expand-account-paths node)) tree)
+             ))))
+
+(defun jacob-ledger-accounts ()
+  (jacob-expand-account-paths (jacob-strip-booleans (jacob-ledger-find-accounts-in-buffer)))
   )
 
 (defun jacob-finance/customize ()
@@ -36,6 +56,24 @@
               ledger-report-reg
               ledger-report-account))
   )
+
+(defun ledger-add-food (title in amount out)
+  (interactive
+   (let ((accounts (mapcar 'list (jacob-ledger-accounts))))
+     (list (read-string "Entry: " (format-time-string "%Y-%m-%d " (current-time)))
+           (let ((completion-regexp-list '("^expenses:")))
+             (completing-read "What did you pay for? " accounts))
+           (read-string "How much did you pay? " " ")
+           (let ((completion-regexp-list '("^\\(assets\\|liabilities\\):")))
+             (completing-read "Where did the money come from? " accounts)))))
+  (message "%s" (type-of amount))
+  (insert title)
+  (newline)
+  (indent-to 4)
+  (insert in "  " amount)
+  (newline)
+  (indent-to 4)
+  (insert out "  " (number-to-string (- (string-to-number amount)))))
 
 (defun jacob-finance/init-ledger-mode ()
   (use-package ledger-mode
@@ -59,6 +97,7 @@
         "R" 'ledger-report
         "t" 'ledger-insert-effective-date
         "y" 'ledger-set-year
+        "f" 'ledger-add-food
         "RET" 'ledger-set-month)
       ;; temporary hack to work-around an issue with evil-define-key
       ;; more info: https://bitbucket.org/lyro/evil/issues/301/evil-define-key-for-minor-mode-does-not
