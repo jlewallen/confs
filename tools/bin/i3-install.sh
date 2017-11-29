535fb295738160b444d07a7aa14e470f9ae8a0fb
Jacob Lewallen <jlewallen@gmail.com>
2017-11-29 09:09:52 -0800

    Permissions, cleanup.

---
+++ tools/bin/i3-install.sh
@@ -0,0 +1,5 @@
+add-apt-repository ppa:aacebedo/libi3ipc-glib
+add-apt-repository ppa:aacebedo/xfce4-i3-workspaces-plugin
+apt-get update
+apt-get install -yy nitrogen i3 libi3ipc-glib xfce4-i3-workspaces-plugin
+
