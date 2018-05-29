34da48a0abc191c2b96615ac2cf062810bdfbaff
Jacob Lewallen <jlewallen@gmail.com>
2018-05-29 09:40:46 -0700

    Adding doom-themes to additional packages

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -86,7 +86,7 @@ This function should only modify configuration layer settings."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(base16-theme)
+   dotspacemacs-additional-packages '(base16-theme doom-themes)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
