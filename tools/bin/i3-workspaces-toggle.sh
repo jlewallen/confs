2055c6bc778a721ef28471a6279ccb30772248e2
Jacob Lewallen <jlewallen@gmail.com>
2018-12-18 16:01:50 -0800

    Get rid of workspace pairings, all toggle between 1 and [2..8]

--- tools/bin/i3-workspaces-toggle.sh
+++ tools/bin/i3-workspaces-toggle.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+previous_file=/tmp/jacob-i3-previous
 active=`i3-msg -t get_workspaces | jq '(.[] | select(.num < 9) | select(.visible) | .num)'`
 
 case $active in
-    1) i3-msg workspace 2 ;;
-    2) i3-msg workspace 1 ;;
-esac
-
-case $active in
-    3) i3-msg workspace 4 ;;
-    4) i3-msg workspace 3 ;;
-esac
-
-case $active in
-    5) i3-msg workspace 6 ;;
-    6) i3-msg workspace 5 ;;
+    1)
+       if [ -f $previous_file ]; then
+           previous=`head $previous_file`
+           rm /tmp/jacob-i3-previous
+           i3-msg workspace $previous
+       else
+           i3-msg workspace 2
+       fi
+       ;;
+    *)
+       echo $active > /tmp/jacob-i3-previous
+       i3-msg workspace 1
+       ;;
 esac
