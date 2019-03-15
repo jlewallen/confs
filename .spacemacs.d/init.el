f8eed3915a09c99b3da5c33a95f2f8f4d3be83f5
Jacob Lewallen <jlewallen@gmail.com>
2019-03-15 14:08:57 -0700

    Adding htmlize.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -85,7 +85,7 @@ This function should only modify configuration layer settings."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(base16-theme doom-themes kaolin-themes)
+   dotspacemacs-additional-packages '(htmlize base16-theme doom-themes kaolin-themes)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
