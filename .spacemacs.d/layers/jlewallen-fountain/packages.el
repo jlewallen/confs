15aa18331bb9670295c701f73dcb11486e3b05a6
Jacob Lewallen <jlewallen@gmail.com>
2019-04-09 14:08:47 -0700

    Playing with fountain-mode

---
+++ .spacemacs.d/layers/jlewallen-fountain/packages.el
@@ -0,0 +1,20 @@
+;;; packages.el --- Finance Layer packages File for Spacemacs
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Sylvain Benner <sylvain.benner@gmail.com>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(setq jlewallen-fountain-packages
+      '(
+        (fountain-mode)
+        )
+      )
+
+(defun jlewallen-fountain/init-fountain-mode ())
+
+(defun jlewallen-fountain/post-init-fountain-mode ())
