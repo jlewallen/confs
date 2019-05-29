d11addec40a1f3d044f4cdd35f36839511c8b3ab
Jacob Lewallen <jlewallen@gmail.com>
2019-05-28 17:33:19 -0700

    A nifty way to get a side by side editor and terminal.

---
+++ tools/bin/i3-toggle-two-up.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+emacs_ws=1
+other_ws=2
+
+# TODO Should we only move terminals?
+
+stealing=`i3-msg -t get_tree | jq ".nodes[] | .. | select(.num?) | select(.num==$other_ws) | .. | select(.type? and .type == \"con\") | .id"`
+returning=`i3-msg -t get_tree | jq ".nodes[] | .. | select(.num?) | select(.num==$emacs_ws) | .. | select(.type? and .type == \"con\") | select(.window_properties.class != \"Emacs\") | .id"`
+emacs_id=`i3-msg -t get_tree | jq ".nodes[] | .. | select(.window_properties? and .window_properties.class == \"Emacs\") | .id"`
+emacs_fs_id=`i3-msg -t get_tree | jq ".nodes[] | .. | select(.window_properties? and .window_properties.class == \"Emacs\" and .fullscreen_mode == 1) | .id"`
+
+if [ -z $returning ]; then
+    echo "stealing workspace 2 tmux"
+
+    i3-msg "workspace 2"
+    for id in $stealing; do
+        echo "stealing $id"
+        i3-msg "[con_id=\"$id\"] move to workspace 1"
+    done
+
+    if [ ! -z "$emacs_fs_id" ]; then
+        i3-msg "workspace 1"
+        i3-msg "[con_id=\"$emacs_fs_id\"] focus"
+        i3-msg "fullscreen toggle"
+    fi
+else
+    echo "returning stolen windows to workspace OTHER"
+
+    for id in $returning; do
+        i3-msg "[con_id=\"$id\"] move to workspace 2"
+        i3-msg "[con_id=\"$id\"] focus"
+        i3-msg "fullscreen toggle"
+    done
+
+    if [ -z "$emacs_fs_id" ]; then
+        i3-msg "workspace 1"
+        i3-msg "[con_id=\"$emacs_id\"] focus"
+        i3-msg "fullscreen toggle"
+    fi
+fi
--- tools/bin/i3-workspaces-toggle.sh
+++ tools/bin/i3-workspaces-toggle.sh
@@ -2,6 +2,7 @@
 
 previous_file=/tmp/jacob-i3-previous
 active=`i3-msg -t get_workspaces | jq '(.[] | select(.num < 9) | select(.visible) | .num)'`
+nearby_terminals=`i3-msg -t get_tree | jq ".nodes[] | .. | select(.num?) | select(.num==$emacs_ws) | .. | select(.type? and .type == \"con\" and .window_properties.instance == \"urxvt\") | .id"`
 
 case $active in
     1)
