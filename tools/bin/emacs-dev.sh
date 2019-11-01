66fe84dc4b24855a843270541c0d8f97bc9b9c87
Jacob Lewallen <jlewallen@gmail.com>
2019-11-01 14:49:37 -0700

    Paths in emacs environment, development script.

---
+++ tools/bin/emacs-dev.sh
@@ -0,0 +1,4 @@
+PATH=$PATH:$HOME/tools/clang+llvm-9.0.0-x86_64-linux-gnu-ubuntu-18.04/bin
+PATH=$PATH:$HOME/tools/ccls/Release
+
+HOME=~/dropbox/emacs-work emacs -q --load "dropbox/emacs-work/start.el" --debug-init
