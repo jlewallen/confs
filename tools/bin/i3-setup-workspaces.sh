b016220d2a96525b2961d950c36ce452b9ba2032
Jacob Lewallen <jlewallen@gmail.com>
2020-09-30 07:52:33 -0700

    Move workspace 5 to side monitor.

--- tools/bin/i3-setup-workspaces.sh
+++ tools/bin/i3-setup-workspaces.sh
@@ -36,10 +36,10 @@ if [ -z $valid ]; then
 fi
 
 i3-msg "workspace number 9, move workspace to output $MONITOR_SIDE"
+i3-msg "workspace number 5, move workspace to output $MONITOR_SIDE"
 
 for n in 2 3 4 5 6 7 8 1; do
     i3-msg "workspace number $n, move workspace to output $MONITOR_MAIN"
 done
 
 i3-msg "reload"
-
