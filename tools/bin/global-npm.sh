28ad65e6ab53e525afa3d71d6c97c17d9d3a8762
Jacob Lewallen <jlewallen@gmail.com>
2020-10-08 14:39:57 -0700

    Add vls to global npm.

--- tools/bin/global-npm.sh
+++ tools/bin/global-npm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm install -g nativescript prettier yarn javascript-typescript-langserver typescript-language-server
+npm install -g nativescript prettier yarn javascript-typescript-langserver typescript-language-server vls
