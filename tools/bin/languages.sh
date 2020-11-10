26f6921eb8b157bb20e02f70ab12155187100ac3
Jacob Lewallen <jlewallen@gmail.com>
2020-11-10 14:53:06 -0800

    Include prettier in global npm install.

--- tools/bin/languages.sh
+++ tools/bin/languages.sh
@@ -3,5 +3,5 @@
 set -xe
 
 go get -u github.com/sourcegraph/go-langserver
-npm install -g typescript-language-server vls
+npm install -g vls prettier prettier-plugin-import-sort
 pip3 install python-language-server
