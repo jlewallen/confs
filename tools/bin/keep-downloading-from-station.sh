0f71da6cf72e3b6dcbdac3a4863a37c2ee0acb7f
Jacob Lewallen <jlewallen@gmail.com>
2021-06-01 09:00:08 -0700

    feat: More download test scripts.

--- tools/bin/keep-downloading-from-station.sh
+++ tools/bin/keep-downloading-from-station.sh
@@ -3,7 +3,6 @@
 set -xe
 
 while true; do
-	echo hello
 	sleep 1
 	curl --connect-timeout 1 --speed-time 5 --speed-limit 1000 http://192.168.0.209/fk/v1/download/data -v > /dev/null
 done
---
+++ tools/bin/keep-querying-status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -xe
+
+while true; do
+	sleep 1
+	fkc --address 192.168.0.209 --status
+done
