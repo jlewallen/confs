6528a1f329a941188c6a99dc773a2123e40bf346
Jacob Lewallen <jlewallen@gmail.com>
2023-10-27 19:38:54 -0700

    Macos needs specific espflash.

--- tools/bin/fresh-cargo-install.sh
+++ tools/bin/fresh-cargo-install.sh
@@ -7,9 +7,12 @@ cargo install sccache
 export RUSTC_WRAPPER=~/.cargo/bin/sccache
 
 for name in du-dust exa git-delta just trunk cargo-watch cargo-expand cargo-edit \
-		ldproxy espup espflash \
+		ldproxy espup \
 		cargo-generate cargo-modules miniserve ; do
 	cargo install $name
 done
+
+# Necessary for Mac only
+cargo install --git https://github.com/SergioGasquez/espflash --branch fix/resets cargo-espflash espflash
 
 hash -r
