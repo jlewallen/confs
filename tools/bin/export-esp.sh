9771d7767b1ccea6e2d78d3aa8ef7a84832538a6
Jacob Lewallen <jlewallen@gmail.com>
2024-06-05 08:00:09 -0700

    Helper scripts.

---
+++ tools/bin/export-esp.sh
@@ -0,0 +1,3 @@
+export LIBCLANG_PATH="/home/jlewallen/.rustup/toolchains/esp/xtensa-esp32-elf-clang/esp-16.0.0-20230516/esp-clang/lib"
+export PATH="/home/jlewallen/.rustup/toolchains/esp/xtensa-esp-elf/esp-13.2.0_20230928/xtensa-esp-elf/bin:$PATH"
+export PATH="/home/jlewallen/.rustup/toolchains/esp/riscv32-esp-elf/esp-13.2.0_20230928/riscv32-esp-elf/bin:$PATH"
