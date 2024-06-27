52f3014c83f800f9b5c0ad854cb38ad8c085d5e8
Jacob Lewallen <jlewallen@gmail.com>
2024-06-27 08:16:48 -0700

    Start tmux sessions script.

---
+++ tools/bin/start-sessions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+setxkbmap -option caps:super
+
+tmux new-session -d -s self
+tmux new-session -d
+tmux new-session -d
+tmux new-session -d
+tmux new-session -d
+tmux new-session -d
+tmux new-session -d -s app
+tmux new-session -d -s cloud
+tmux new-session -d
+tmux new-session -d
+tmux new-session -d -s vr
